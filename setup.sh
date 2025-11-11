c02949c8513a6f86bf05692d32b3f77c707be0d1
Nicholas.Ho <j.xiong@live.com>
2025-11-11 23:56:45 +0000

    Added ghcup setup

--- setup.sh
+++ setup.sh
@@ -52,6 +52,13 @@ curl -LO https://github.com/wfxr/code-minimap/releases/download/v0.6.8/code-mini
 sudo dpkg -i code-minimap_0.6.8_amd64.deb
 
 
+# === Install compilers and build tools ===
+
+# Haskell (ghcup)
+sudo apt install -y build-essential curl libffi-dev libffi8ubuntu1 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_GHC_VERSION=latest BOOTSTRAP_HASKELL_CABAL_VERSION=latest BOOTSTRAP_HASKELL_INSTALL_STACK=1 BOOTSTRAP_HASKELL_INSTALL_HLS=1 sh
+
+
 # === Install language servers ===
 
 sudo apt update
