18ae775186a5ce70efe936c23fb6ff7c985d76df
Nicholas.Ho <j.xiong@live.com>
2025-11-12 00:09:47 +0000

    Added gcc setup (prerequisite)

--- setup.sh
+++ setup.sh
@@ -54,6 +54,9 @@ sudo dpkg -i code-minimap_0.6.8_amd64.deb
 
 # === Install compilers and build tools ===
 
+# gcc (C/C++)
+sudo apt install gcc
+
 # Haskell (ghcup)
 sudo apt install -y build-essential curl libffi-dev libffi8ubuntu1 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_GHC_VERSION=latest BOOTSTRAP_HASKELL_CABAL_VERSION=latest BOOTSTRAP_HASKELL_INSTALL_STACK=1 BOOTSTRAP_HASKELL_INSTALL_HLS=1 sh
